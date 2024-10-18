51a2f927d1094819117ab3eb849863a8188ce7a5
Abel Aghorighor <52415319+aaghorighor@users.noreply.github.com>
2024-10-18 15:24:57 +0100

    added more unit test

--- ___test___/fetchCat.test.tsx
+++ ___test___/fetchCat.test.tsx
@@ -4,23 +4,28 @@ import { useCatImages } from "../hooks/useCatImages";
 
 jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
-jest.mock('expo-constants', () => ({
-    expoConfig: jest.fn() 
+jest.mock("expo-constants", () => ({
+    expoConfig: {
+        extra: {
+            apiKey: "mock-api-key",
+            apiUrl: "mock-api-url"
+        },
+    },
 }));
 
 const mockCats = [
-	{
-		id: "1",
-		url: "https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg",
-		height: 300,
-		width: 300,
-	},
-	{
-		id: "2",
-		url: "https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg",
-		height: 300,
-		width: 300,
-	},
+    {
+        id: "1",
+        url: "https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg",
+        height: 300,
+        width: 300,
+    },
+    {
+        id: "2",
+        url: "https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg",
+        height: 300,
+        width: 300,
+    },
 ];
 
 describe('useCatImages Hook', () => {
@@ -47,5 +52,46 @@ describe('useCatImages Hook', () => {
         expect(result.current.data).not.toEqual([]);
         expect(result.current.error).toBeNull();
 
+    });
+
+    it("should fetch cat images successfully", async () => {
+
+        mockedAxios.get.mockResolvedValueOnce({ data: mockCats });
+
+        const { result, waitForNextUpdate } = renderHook(() =>
+            useCatImages(),
+        );
+
+        await waitForNextUpdate();
+
+        expect(axios.get).toHaveBeenCalledWith(
+            `${"mock-api-url"}/v1/images/search`,
+            {
+                headers: {
+                    "x-api-key": "mock-api-key",
+                },
+                params: {
+                    limit: 10,
+                    page: 1,
+                },
+            },
+        );
+        expect(result.current.loading).toBe(false);
+        expect(result.current.data).toEqual(mockCats);
+        expect(result.current.error).toBe(null);
+    });
+
+    it("should handle error if fetching fails", async () => {
+        mockedAxios.get.mockRejectedValueOnce(new Error("Network Error"));
+
+        const { result, waitForNextUpdate } = renderHook(() =>
+            useCatImages(),
+        );
+
+        await waitForNextUpdate();
+
+        expect(result.current.loading).toBe(false);
+        expect(result.current.data).toEqual([]);
+        expect(result.current.error).toEqual(new Error("Failed to fetch cat images. Please try again."));
     });
 })
